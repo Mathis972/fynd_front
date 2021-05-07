a6269f4135e0be4bdc563a447ca3434c0fb9a5ab
jbdonin <jean-baptiste.38@hotmail.com>
2021-05-07 09:28:50 +0200

    ajout du socket.io

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import VueSocketIO from 'vue-socket.io'
+import SocketIO from 'socket.io-client'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+
+const socketConnection = SocketIO('http://localhost:8000')
+
+Vue.use(VueMaterial)
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: socketConnection
+})
+)
 Vue.config.productionTip = false
 
 new Vue({
