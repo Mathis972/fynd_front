0bcc7b230fc75edde5de89c627f6a7aca87c1f30
Léo ROUILLARD <leorouillard@gmail.com>
2021-05-07 10:35:15 +0200

    correction erreur view app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,3 @@
 <template>
-  <HelloWorld/>
+  <router-view/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld'
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
-}
-</script>
