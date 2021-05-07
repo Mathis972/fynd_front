0e211f8ff6a1c3132246a360f943c6c18c37daeb
Léo ROUILLARD <leorouillard@gmail.com>
2021-05-07 10:46:28 +0200

    création fichier component et view + route pour page quizz

---
+++ src/components/QuestionsQuizz.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="quizz">
+    <h2>Quizz</h2>
+    <p class="text-presentation">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, quisquam! Impedit et praesentium pariatur quod, dicta facilis eaque vitae eos quam explicabo vel. Repellat itaque magnam expedita deleniti consectetur nam!</p>
+    <p class="text-presentation">Laborum deserunt unde pariatur delectus maxime in expedita nesciunt beatae voluptates recusandae vero amet repellendus. Velit.</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'QuestionsQuizz'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#quizz {
+  margin-top: 20px;
+}
+
+body {
+  font-family: 'Segoe UI';
+}
+
+h3, h2 {
+  text-align: center;
+  color: white;
+}
+</style>
---
+++ src/views/Quizz.vue
@@ -0,0 +1,21 @@
+<template>
+    <QuestionsQuizz />
+</template>
+
+<script>
+// @ is an alias to /src
+import QuestionsQuizz from '@/components/QuestionsQuizz.vue'
+
+export default {
+  name: 'Quizz',
+  components: {
+    QuestionsQuizz
+  }
+}
+</script>
+
+<style>
+body {
+  background: linear-gradient(90deg, #de268e 0%, #d57c29 100%, #d57e27 103.36%);
+}
+</style>
