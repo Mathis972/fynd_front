4906ca814c2acdcae685d40bce311b49fb2c73ce
doninj <jean-baptiste.38@hotmail.com>
2021-05-25 09:26:57 +0200

    modification des noms

---
+++ src/views/Connexion.vue
@@ -0,0 +1,21 @@
+<template>
+    <Connexion />
+</template>
+
+<script>
+// @ is an alias to /src
+import Connexion from '@/components/Connexion.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Connexion
+  }
+}
+</script>
+
+<style>
+body {
+  background: linear-gradient(90deg, #de268e 0%, #d57c29 100%, #d57e27 103.36%);
+}
+</style>
